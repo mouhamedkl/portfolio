993ab3239199dc3883d655489355f4e839356e98
mouhamedkl <mohamedamine.klai@esprit.tn>
2025-01-13 00:47:59 +0100

    fix contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,8 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit{
   contactForm!: FormGroup;
-  formSubmitted = false;
-
+  isLoading: boolean = false; // Variable to track loading state
   constructor(private fb: FormBuilder,private service:EmailService) {
    
   }
@@ -19,19 +18,22 @@ export class ContactComponent implements OnInit{
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required,Validators.pattern("^[0-9]+$")],
+      num: ['', [Validators.required,Validators.pattern("^[0-9]+$")]],
       message: ['', Validators.required],
     });
   }
 
-  contact=new Contact();
+ 
 
   submitForm() {
     if (this.contactForm.valid) {
-      this.service.sendEmail(this.contact).subscribe(data=>{
+      this.isLoading = true; // Start loading
+      this.service.sendEmail(this.contactForm.value).subscribe(data=>{
+        this.isLoading = false; // Stop loading
        alert("Email sent successfully")
        this.contactForm.reset();
        },(error)=>{ 
+        this.isLoading = false; // Stop loading
          alert("Error sending email")
         }
       );
@@ -45,6 +47,6 @@ export class ContactComponent implements OnInit{
   }
   get name() { return this.contactForm.get('name'); }
   get email() { return this.contactForm.get('email'); }
-  get phone() { return this.contactForm.get('phone'); }
+  get phone() { return this.contactForm.get('num'); }
   get message() { return this.contactForm.get('message'); }
 }
--- src/app/shared/email.service.ts
+++ src/app/shared/email.service.ts
@@ -10,6 +10,6 @@ export class EmailService {
    url="https://back-chi-three.vercel.app/api/v1/emails"
    sendEmail(data:Contact){
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this.http.post<any>(this.url , data,{headers:headers,responseType: 'json'});
+    return this.http.post<any>(this.url , data);
    }
 }
